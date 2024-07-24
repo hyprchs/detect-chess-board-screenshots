1f420b3e54d2538b8f216731469e72883b458baa
Jackson Hall <jacksonthall22@gmail.com>
2024-07-24 13:46:37 -0400

    Add README, `main.sh`; improve options w/ `argparse`

--- synthetic_data_creation/generate_fens.py
+++ synthetic_data_creation/generate_fens.py
@@ -2,34 +2,51 @@ import chess
 from chess.pgn import read_game
 import io
 from time import perf_counter
+import argparse
 
 # NOTE - Download and extract one of the `.pgn.zst` files at https://database.lichess.org/
-input_pgn = 'lichess_db_standard_rated_2013-01.pgn'
-output_file = 'fens.json'
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--input_pgn",
+    type=str,
+    default="lichess_db_standard_rated_2013-01.pgn",
+    help="Path to the input PGN file",
+)
+args = parser.parse_args()
+
+OUTPUT_FILE = "fens.json"
 
 start_time = perf_counter()
 
-with open(input_pgn, 'r') as f:
-  strio = io.StringIO(f.read())
+try:
+    with open(args.input_pgn, "r") as f:
+        strio = io.StringIO(f.read())
+except FileNotFoundError:
+    print(
+        f"File not found: {args.input_pgn}. Please download a PGN file from https://database.lichess.org/ 
+        and extract it. A very small file can generate a large number of FENs, so ex. try 
+        https://database.lichess.org/standard/lichess_db_standard_rated_2013-01.pgn.zst."
+    )
+    exit(1)
 
 MAX_GAMES = 5_000
 games = []
 games_read = 0
 while True:
-  g = read_game(strio)
-  if g is None:
-    break
+    g = read_game(strio)
+    if g is None:
+        break
 
-  games.append(g)
-  games_read += 1
+    games.append(g)
+    games_read += 1
 
-  if games_read % 1_000 == 0:
-    print(f'Read {games_read} games')
+    if games_read % 1_000 == 0:
+        print(f"Read {games_read} games")
 
-  if games_read == MAX_GAMES:
-    break
+    if games_read == MAX_GAMES:
+        break
 
-print(f'Read {len(games)} games in {perf_counter() - start_time:.2f} seconds')
+print(f"Read {len(games)} games in {perf_counter() - start_time:.2f} seconds")
 
 # Until we get 10_000 FENs, do the following:
 #  - Get a random game from `games`
@@ -61,24 +78,26 @@ while len(fenData) < MAX_FENS:
 
     # Add data about this position
     nextFen = {
-      "fen": node.board().fen(),
+        "fen": node.board().fen(),
     }
 
     # Get the last move if we're not in the root node
     if node.move is not None:
-        nextFen['lastMove'] = node.move.uci()
+        nextFen["lastMove"] = node.move.uci()
 
     # Get the check square if the position is in check
     b = node.board()
     if b.is_check():
-        nextFen['check'] = chess.square_name(next(iter(chess.SquareSet(b.kings & b.occupied_co[b.turn]))))
+        nextFen["check"] = chess.square_name(
+            next(iter(chess.SquareSet(b.kings & b.occupied_co[b.turn])))
+        )
 
     fenData.append(nextFen)
 
     if len(fenData) % 1_000 == 0:
-        print(f'Generated {len(fenData)} FENs')
+        print(f"Generated {len(fenData)} FENs")
 
-with open(output_file, 'w') as f:
-  json.dump(fenData, f)
+with open(OUTPUT_FILE, "w") as f:
+    json.dump(fenData, f)
 
-print(f'Generated {len(fenData)} FENs in {perf_counter() - start_time:.2f} seconds')
+print(f"Generated {len(fenData)} FENs in {perf_counter() - start_time:.2f} seconds")
--- synthetic_data_creation/main.py
+++ synthetic_data_creation/main.py
@@ -5,20 +5,27 @@ from PIL import Image
 from io import BytesIO
 from tqdm import tqdm
 import json
+import argparse
 
 # Directories and file paths
-images_dir = "resized_images/"
-fens_file_path = "fens.json"
-output_dir = "output/"
-
-NUM_TO_GENERATE = 1500
+IMAGES_DIR = "resized_images/"
+FENS_FILE_PATH = "fens.json"
 
 
-with open(fens_file_path, "r") as f:
-    fens = json.load(f)
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--num", "-n", type=int, default=1000, help="Number of images to generate")
+parser.add_argument("--image-output-dir", type=str, default="output/images", help="Output directory for images")
+parser.add_argument("--bbox-output-dir", type=str, default="output/bounding_boxes", help="Output directory for bounding boxes")
+args = parser.parse_args()
 
 # Ensure the output directory exists
-os.makedirs(output_dir, exist_ok=True)
+os.makedirs(args.image_output_dir, exist_ok=True)
+os.makedirs(args.bbox_output_dir, exist_ok=True)
+
+# Get the generated FENs
+with open(FENS_FILE_PATH, "r") as f:
+    fens = json.load(f)
 
 
 # Function to randomly sample an image from the images directory
@@ -61,11 +68,14 @@ def overlay_images(background_image, chessboard_image):
 
 
 # Function to save the image and bounding box
-def save_image_and_bounding_box(image, bounding_box, output_dir, image_name):
-    output_image_path = os.path.join(output_dir, image_name)
+def save_image_and_bounding_box(image, bounding_box, image_name):
+    output_image_path = os.path.join(args.image_output_dir, image_name)
+    bounding_box_path = os.path.join(
+        args.bbox_output_dir, image_name.replace(".png", ".txt")
+    )
+
     image.save(output_image_path)
 
-    bounding_box_path = output_image_path.replace(".png", ".txt")
     with open(bounding_box_path, "w") as bbox_file:
         bbox_file.write(
             f"{bounding_box[0]},{bounding_box[1]},{bounding_box[2]},{bounding_box[3]}"
@@ -73,26 +83,22 @@ def save_image_and_bounding_box(image, bounding_box, output_dir, image_name):
 
 
 # Main script logic
-for i in tqdm(
-    range(NUM_TO_GENERATE)
-):  # Adjust the number of synthetic images you want to generate
+for i in tqdm(range(args.num)):
     try:
-        background_image_name = sample_image(images_dir)
-        background_image_path = os.path.join(images_dir, background_image_name)
+        background_image_name = sample_image(IMAGES_DIR)
+        background_image_path = os.path.join(IMAGES_DIR, background_image_name)
         background_image = Image.open(background_image_path).convert("RGBA")
 
         fenData = sample_fen()
 
-        chessboard_image = generate_chessboard_image(fenData | {'colors': 'random'})
+        chessboard_image = generate_chessboard_image(fenData | {"colors": "random"})
 
         overlayed_image, bounding_box = overlay_images(
             background_image, chessboard_image
         )
 
         output_image_name = f"synthetic_{i}.png"
-        save_image_and_bounding_box(
-            overlayed_image, bounding_box, output_dir, output_image_name
-        )
+        save_image_and_bounding_box(overlayed_image, bounding_box, output_image_name)
     except Exception as e:
         print(f"Error generating synthetic image {i}: {e}")
 
